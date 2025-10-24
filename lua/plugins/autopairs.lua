82d1606141a6cb6580f395c2b7e43753e09af829
nobel-von-it <maksimdavydenko12@gmail.com>
2025-10-24 08:21:19 +0700

    fix: user input answer

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,7 +1,5 @@
 return {
-    'windwp/nvim-autopairs',
-    event = "InsertEnter",
-    opts = {}
-    -- use opts = {} for passing setup options
-    -- this is equivalent to setup({}) function
+	"windwp/nvim-autopairs",
+	event = "InsertEnter",
+	opts = {},
 }
