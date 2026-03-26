222874c5e578772d6bffa09256c8e3b87836f1d9
nobel-von-it <maksimdavydenko12@gmail.com>
2026-03-26 22:49:28 +0700

    fix(env): gpg variable

--- init.lua
+++ init.lua
@@ -1,1 +1,19 @@
+local function get_fish_env(var_name)
+    local cmd = string.format("fish -c 'unlock_vault > /dev/null; echo $%s'", var_name)
+    local handle = io.popen(cmd)
+    local result = handle:read("*a")
+    handle:close()
+    return result:gsub("%s+", "")
+end
+
+local gpg_home = get_fish_env("GNUPGHOME")
+if gpg_home ~= "" then
+    vim.env.GNUPGHOME = gpg_home
+end
+
+local ssh_auth = get_fish_env("SSH_AUTH_SOCK")
+if ssh_auth ~= "" then
+    vim.env.SSH_AUTH_SOCK = ssh_auth
+end
+
 require("config.lazy")
